ca885da656b9a54d0752abe499b9ff46a5d39cbd
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-08-12 20:31:21 +0200

    Refactor app.py: Clean comments and remove redundant empty docstring
    Removed unnecessary comments and an empty docstring in the /mine route. This enhances code readability and maintains cleaner code practices without affecting functionality.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from blockchain import Blockchain
 # Instantiate our Node
 app = Flask(__name__)
 
-# Generate a globally unique address for this node
+
 node_identifier = str(uuid4()).replace('-', '')
 
 # Instantiate the Blockchain
@@ -13,23 +13,19 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    """
 
-    """
-    # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
     proof = blockchain.proof_of_work(last_proof)
 
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
+
     blockchain.new_transaction(
         sender="0",
         recipient=node_identifier,
         vote=None,
     )
 
-    # Forge the new Block by adding it to the chain
+
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
 
